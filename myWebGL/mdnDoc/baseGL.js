b6622d0c28dfd168630fa57b865ff3ebb9a82592
苏泽雄 <734531289@qq.com>
2019-11-07 13:53:09 +0800

    trigle

--- myWebGL/mdnDoc/baseGL.js
+++ myWebGL/mdnDoc/baseGL.js
@@ -1,29 +1,38 @@
 function createShaderFromScript(gl, type, id) {
-    let shaderSource = document.querySelector(`#${id}`).innerHTML;
-    // 创建顶点着色器对象
-    let shader = gl.createShader(type);
-    // 将源码分配给顶点着色器对象
-    gl.shaderSource(shader, shaderSource);
-    // 编译顶点着色器程序
-    gl.compileShader(shader);
-    return shader;
+  let shaderSource = document.querySelector(`#${id}`).innerHTML;
+  // 创建顶点着色器对象
+  let shader = gl.createShader(type);
+  // 将源码分配给顶点着色器对象
+  gl.shaderSource(shader, shaderSource);
+  // 编译顶点着色器程序
+  gl.compileShader(shader);
+  return shader;
 }
+
 function createProgram(gl, vertexShader, fragmentShader) {
-    let program = gl.createProgram();
-    //将顶点着色器挂载在着色器程序上。
-    gl.attachShader(program, vertexShader);
-    //将片元着色器挂载在着色器程序上。
-    gl.attachShader(program, fragmentShader);
-    //链接着色器程序
-    gl.linkProgram(program);
-    return program;
+  let program = gl.createProgram();
+  //将顶点着色器挂载在着色器程序上。
+  gl.attachShader(program, vertexShader);
+  //将片元着色器挂载在着色器程序上。
+  gl.attachShader(program, fragmentShader);
+  //链接着色器程序
+  gl.linkProgram(program);
+  return program;
 }
+
 function randomColor() {
-    const random = Math.random
-    return {
-        r: random() * 255,
-        g: random() * 255,
-        b: random() * 255,
-        a: random() * 1
-    };
+  const random = Math.random
+  return {
+    r: random() * 255,
+    g: random() * 255,
+    b: random() * 255,
+    a: random() * 1
+  };
+}
+
+function createBuffer(gl, attrib, setting) {
+  const buffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.vertexAttribPointer(attrib, setting.size, setting.type, setting.normalize, setting.stribe, setting.offset);
+  return buffer;
 }
